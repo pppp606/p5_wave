a3a62a78919067f2aea501c5bc1dfe2efae41b8f
pppp606 <ppppp303@gmail.com>
2015-07-26 15:15:31 +0900

    add oversampling

--- js/wave.js
+++ js/wave.js
@@ -4,16 +4,11 @@ var wave = {
     volume:50,//ボリューム
     phase:0,//初期位相
     diff:0,//1サンプルで移動する位相量
-<<<<<<< HEAD
-    type:"sin"//波形タイプ(sin,saw,squ,tri)
-  },
-=======
     type:"sin",//波形タイプ(sin,saw,squ,tri)
     sample:0,//サンプリングレート
     multiple:4//オーバーサンプリングの倍数
   },
   buffer:[],
->>>>>>> master
   getY:function(){
     var y;
     this.Set.phase += this.Set.diff;
@@ -42,11 +37,8 @@ var wave = {
 
 function setup() {
   createCanvas(800, 120);
-<<<<<<< HEAD
-=======
   //サンプリングレートを設定
   wave.Set.sample = width * wave.Set.multiple;
->>>>>>> master
 }
 
 function draw() {
@@ -59,15 +51,6 @@ function draw() {
   // スタート時点のy座標
   var lastpoints = wave.getY();
   //周波数でサンプリング数で割る
-<<<<<<< HEAD
-  wave.Set.diff = TWO_PI * wave.Set.frequency / width;
-  //サンプル（横幅）分繰り返す
-  for (var i = 1; i < width; i++) {
-    var y = wave.getY();
-    //波形描画
-    line(i-1,lastpoints,i,y);
-    lastpoints = y;
-=======
   wave.Set.diff = TWO_PI * wave.Set.frequency / wave.Set.sample;
   var i = 1;
   while (i < wave.Set.sample) {
@@ -87,8 +70,6 @@ function draw() {
       line(i/wave.Set.multiple-1,lastpoints,i/wave.Set.multiple,y);
       lastpoints = y;
     }
-
     i = (i + 1)|0;
->>>>>>> master
   }
 }
